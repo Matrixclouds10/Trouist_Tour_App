021d99ec836e2deaa15e60fa374ff6e04689a5ef
Yahia <Yehyaelzaeem>
2024-04-22 11:55:03 +0200

    payment

---
+++ lib/feature/home/data/models/show_payment_response.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'show_payment_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ShowPaymentResponse _$ShowPaymentResponseFromJson(Map<String, dynamic> json) =>
+    ShowPaymentResponse(
+      json['payment'] as String?,
+    );
+
+Map<String, dynamic> _$ShowPaymentResponseToJson(
+        ShowPaymentResponse instance) =>
+    <String, dynamic>{
+      'payment': instance.payment,
+    };
